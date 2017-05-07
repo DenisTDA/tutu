66d464f0eb6b63ac78afabfd8a6d32a3da86a8e0
Denis <deonis_t@mail.ru>
2017-05-08 02:03:11 +0500

    Task 13 second correction

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,7 +1,7 @@
 class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
-  has_many :tickets inverse_of: 'start_station'
-  has_many :tickets inverse_of: 'end_station'
+  has_many :start_station_tickets, class_name: 'Ticket', foreign_key: :start_station_id
+  has_many :end_station_tickets, class_name: 'Ticket', foreign_key: :end_station_id
   has_many :trains
 end
