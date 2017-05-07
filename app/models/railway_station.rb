311be31a107aca4ce10d992773d265dd1ed806c1
Denis <deonis_t@mail.ru>
2017-05-07 18:39:05 +0500

    Task 13 with first correction

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,6 +1,7 @@
 class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
-  has_many :tickets
+  has_many :tickets inverse_of: 'start_station'
+  has_many :tickets inverse_of: 'end_station'
   has_many :trains
 end
