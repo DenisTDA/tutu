10bcb2149d6b053eb8d519c0be111ec2f15dac71
Denis <deonis_t@mail.ru>
2017-06-28 23:26:35 +0500

    task19, don't works devise's locale & bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-
   def after_sign_in_path_for(resource)
     if current_user.admin?
       admin_dashboard_path
@@ -11,8 +10,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
- protected
-
+  protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :email, :password, :password_confirmation])
   end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,10 @@
 class SearchesController < ApplicationController
- # before_action :authenticate_user!, only: [:create]  
+
   def show
   end
 
   def create
-   @trains = Train.search(search_params[:start_station_id], 
+    @trains = Train.search(search_params[:start_station_id], 
                                   search_params[:end_station_id])
     @start_station = RailwayStation.find(search_params[:start_station_id])
     @end_station = RailwayStation.find(search_params[:end_station_id])
@@ -15,6 +15,7 @@ class SearchesController < ApplicationController
   end
 
   private
+ 
   def search_params
     params.require(:stations).permit(:start_station_id, :end_station_id)
   end
