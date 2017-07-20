d04ab3f5ec86429519b0a32a792c1ca7ea145167
Denis Toritsyn <deonis_t@mail.ru>
2017-07-20 17:14:30 +0500

    edit gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
     port: 587,
-    domain: "morning-retreat-91305.herokuapp.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
